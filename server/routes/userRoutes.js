c607f42db4fe6ae3e531151d2ea47c2cef7a2e21
Sagar Embadi <sagarembadi7@gmail.com>
2025-04-11 18:12:38 +0530

    updating server files

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,132 +1,136 @@
-import express from 'express';
-import { User } from '../models/user.js'; 
-import {middleware} from './middleware.js';
+import express from "express";
+import { User } from "../models/user.js";
+import { middleware } from "./middleware.js";
 const router = express.Router();
-import jwt from "jsonwebtoken"
+import jwt from "jsonwebtoken";
 // Create a new user
-router.post('/users', async (req, res) => {
-    try{
-        const {username,email,password,confirmpassword} = req.body;
-        let exist = await User.findOne({email})
-        if(exist){
-            return res.status(400).send('User Already Exist')
-        }
-        if(password !== confirmpassword){
-            return res.status(400).send('Passwords are not matching');
-        }
-        let newUser = new User({
-            username,
-            email,
-            password,
-            confirmpassword
-        })
-        await newUser.save();
-        res.status(200).send('Registered Successfully')
-
+router.post("/users", async (req, res) => {
+  try {
+    const { username, email, password, confirmpassword } = req.body;
+    let exist = await User.findOne({ email });
+    if (exist) {
+      return res.status(400).send("User Already Exist");
     }
-    catch(err){
-        console.log(err)
-        return res.status(500).send('Internel Server Error')
+    if (password !== confirmpassword) {
+      return res.status(400).send("Passwords are not matching");
     }
+    let newUser = new User({
+      username,
+      email,
+      password,
+      confirmpassword,
+    });
+    await newUser.save();
+    res.status(200).send("Registered Successfully");
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Internel Server Error");
+  }
 });
 // login the existing user
-router.post('/login',async (req, res) => {
-    try{
-        const {email,password} = req.body;
-        let exist = await User.findOne({email});
-        if(!exist) {
-            return res.status(400).send('User Not Found');
-        }
-        if(exist.password !== password) {
-            return res.status(400).send('Invalid credentials');
-        }
-        let loggeruser = exist
-        await loggeruser.save()
-        let payload = {
-            user:{
-                id : exist.id
-            }
-        }
-        jwt.sign(payload,'jwtSecret',{expiresIn:3600000},
-          (err,token) =>{
-              if (err) throw err;
-              return res.json({token})
-
-          }  
-            )
-
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    let exist = await User.findOne({ email });
+    if (!exist) {
+      return res.status(400).send("User Not Found");
     }
-    catch(err){
-        console.log(err);
-        return res.status(500).send('Server Error')
+    if (exist.password !== password) {
+      return res.status(400).send("Invalid credentials");
     }
-})
+    let loggeruser = exist;
+    await loggeruser.save();
+    let payload = {
+      user: {
+        id: exist.id,
+      },
+    };
+    jwt.sign(payload, "jwtSecret", { expiresIn: 3600000 }, (err, token) => {
+      if (err) throw err;
+      return res.json({ token });
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Server Error");
+  }
+});
 
 //getting the login user
-router.get('/loggeduser',middleware,async(req, res)=>{
-    try{
-        let exist = await User.findById(req.user.id);
-        if(!exist){
-            return res.status(400).send('User not found');
-        }
-        res.json(exist);
+router.get("/loggeduser", middleware, async (req, res) => {
+  try {
+    let exist = await User.findById(req.user.id);
+    if (!exist) {
+      return res.status(400).send("User not found");
     }
-    catch(err){
-        console.log(err);
-        return res.status(500).send('Server Error')
-    }
-})
+    res.json(exist);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send("Server Error");
+  }
+});
 
 // Get all users
-router.get('/users', async (req, res) => {
-    try {
-        const users = await User.find();
-        res.send(users);
-    } catch (error) {
-        res.status(500).send(error);
-    }
+router.get("/users", async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 // Get a user by ID
-router.get('/users/:id', async (req, res) => {
-    try {
-        let user = await User.findById(req.params.id).populate("followers","username profilePicture").populate("following","username profilePicture").populate('saved', 'title media location author likes comments createdAt updatedAt');
-        user = await user.populate('saved.author','username profilePicture')
-        user = await user.populate("saved.likes",'username profilePicture')
-        user = await user.populate("saved.comments.user",'username profilePicture')
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (error) {
-        res.status(500).send(error);
+router.get("/users/:id", async (req, res) => {
+  try {
+    let user = await User.findById(req.params.id)
+      .populate("followers", "username profilePicture")
+      .populate("following", "username profilePicture")
+      .populate(
+        "saved",
+        "title media location author likes comments createdAt updatedAt"
+      );
+    user = await user.populate("saved.author", "username profilePicture");
+    user = await user.populate("saved.likes", "username profilePicture");
+    user = await user.populate(
+      "saved.comments.user",
+      "username profilePicture"
+    );
+    if (!user) {
+      return res.status(404).send();
     }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 // Update a user by ID
-router.patch('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-        if (!user) {
-            return res.status(404).send("User Not Found");
-        }
-        res.send(user);
-    } catch (error) {
-        res.status(400).send(error);
+router.patch("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!user) {
+      return res.status(404).send("User Not Found");
     }
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error);
+  }
 });
 
 // Delete a user by ID
-router.delete('/users/:id', async (req, res) => {
-    try {
-        const user = await User.findByIdAndDelete(req.params.id);
-        if (!user) {
-            return res.status(404).send();
-        }
-        res.send(user);
-    } catch (error) {
-        res.status(500).send(error);
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+    if (!user) {
+      return res.status(404).send();
     }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
 });
 
 // Use named export
